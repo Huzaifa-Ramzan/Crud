ac7383fc4a84fa7e1f93aa4d4aaced1f8c84f91c
Huzaifa Ramzan <84973513+Huzaifa-Ramzan@users.noreply.github.com>
2021-09-08 19:36:12 +0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css" >
+	<link rel="stylesheet" href="./CSS/style.css">
+	<!--fontawesome-->
+	<link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <title>crud_1</title>
+  </head>
+  <body class="text-white">
+	  <!--navigation-->
+	  <nav class="navbar navbar-expand navbar-dark p-4"   >
+		  <div class="container" style="background-color:#1f1f1f;">
+		
+			  <div class="collapse navbar-collapse " id="mynav">
+				  <ul class="navbar-nav">
+					  <li class="nav-item px-4">
+						  <a href="#project" class="nav-link ">Home</a>
+					  </li>
+					  <li class="nav-item  px-4">
+						  <a href="#about" class="nav-link">Save</a>
+					  </li>
+					  <li class="nav-item px-4">
+						  <a href="#footer" class="nav-link ">Contact</a>
+					  </li>
+				  </ul>
+			  </div>
+		  </div>
+	  </nav>
+
+
+
+
+
+
+
+
+	  <script src="./css/jquery.min.js"></script>
+
+
+	  <!--Optional JavaScript; choose one of the two! -->
+	
+	  <!-- Option 1: Bootstrap Bundle with Popper -->
+	  <script src="../css/bootstrap.bundle.js"></script>
+	  <!-- Option 2: Separate Popper and Bootstrap JS -->
+	 
+	  <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+	  -->
